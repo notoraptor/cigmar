b53356b1d34f8b406ae6d1d7bce2975e43027f5f
notoraptor <stevenbocco@gmail.com>
2017-08-25 16:47:16 -0400

    update

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 int main() {
 	cout << boolalpha;
-	cout << "Hello World!" << endl;
+	array_t<int, 24> l;
+	int start = 0;
+	int sum = 0;
+	size_t prod = 1;
+	l.each([&start](int& i) {i = ++start;});
+	l.each([&sum](int& i) {sum += i;});
+	l.each([&prod](int& i) {prod *= i;});
+	cout << "hello" << endl;
+	l.each([](int& i) {cout << i << endl;});
+	cout << "sum:  " << sum  << endl;
+	cout << "prod: " << prod << endl;
 	return 0;
 }
--- primitive_classes.hpp
+++ primitive_classes.hpp
@@ -1,10 +1,39 @@
 #ifndef CIGMAR_PRIMITIVE_CLASSES
 #define CIGMAR_PRIMITIVE_CLASSES
 
+#include <array>
+
 class AbstractMap;
 class AbstractSet;
 
-class StaticArray; // C++ array -> array
+template<typename T, size_t N>
+class array_t {
+private:
+    T mem[N];
+public:
+    const size_t length;
+public:
+    array_t(): length(N) {}
+    array_t(const array_t&) = delete;
+
+    T& operator[](size_t pos) {return mem[pos];}
+    const T& operator[](size_t pos) const {return mem[pos];}
+
+    template<typename F>
+    array_t& each(F func) {
+        for (size_t i = 0; i < length; ++i) func(mem[i]);
+        return *this;
+    }
+    template<typename F>
+    const array_t& each(F func) const {
+        for (size_t i = 0; i < length; ++i) func(mem[i]);
+        return *this;
+    }
+    array_t& memset(byte v) {
+        memset(mem, v, length);
+    }
+};
+
 class Array; // -> vector
 class LinkedList; // Double linked list.
 class Stack;
