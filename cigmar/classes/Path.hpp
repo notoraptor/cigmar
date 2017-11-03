b191a557cbec823f1137251a3ed580417db11f36
notoraptor <stevenbocco@gmail.com>
2017-11-03 09:56:48 -0400

    Up

---
+++ cigmar/classes/Path.hpp
@@ -0,0 +1,38 @@
+#ifndef CIGMAR_PATH_HPP_INCLUDED
+#define CIGMAR_PATH_HPP_INCLUDED
+
+#include <bitset>
+#include <cigmar/filesystem.hpp>
+
+namespace cigmar::sys {
+
+class Path {
+private:
+	enum class Status {TYPE_CHECKED, EXTENSION_CHECKED, IS_NORMALIZED, IS_ABSOLUTE, COUNT};
+	Path* parent;
+	String path;
+public:
+	Path(const char* p);
+	Path(const String& p);
+	Path(const Path& parent, const char* p);
+	Path(const Path& parent, const String& p);
+	Path(const Path& p);
+	Path(Path&& p);
+	bool exists() const;
+	bool isFile() const;
+	bool isDirectory() const;
+	bool hasExtension() const;
+	Path absolute() const;
+	Path normalized() const;
+	Path parent() const;
+	String filename() const;
+	String basename() const;
+	String extension() const;
+	operator const String&() const;
+	Path& resolve();
+};
+
+}
+
+
+#endif // CIGMAR_PATH_HPP_INCLUDED
