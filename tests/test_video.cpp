892fdb4b0c1fe01bf33661741b2af7a25d8e96e1
notoraptor <stevenbocco@gmail.com>
2017-11-09 14:03:23 -0500

    Add quick test to check new videos filenames.

--- tests/test_video.cpp
+++ tests/test_video.cpp
@@ -1,5 +1,7 @@
 #include <cigmar/unittests.hpp>
 #include <cigmar/classes/String.hpp>
+#include <cigmar/filesystem.hpp>
+#include <cigmar/print.hpp>
 #include <video/Video.hpp>
 using namespace cigmar;
 
@@ -10,22 +12,32 @@ Tested file: tests/Lion.ogv
 
 utt_begin(TestVideo);
 
-utt(test_video) {
-	String folder = "tests";
-	String filename = "Lion.ogv";
-	video::Video v(sys::path::join(folder, filename));
-	utt_assert(v.getFilename() == sys::path::join(folder, filename));
-	utt_assert(v.getAudioCodec() == "vorbis");
-	utt_assert(v.getVideoCodec() == "theora");
-	utt_assert(v.getFormat() == "Ogg");
-	utt_assert(v.getFrameRate() == 25);
-	utt_assert(v.getSampleRate() == 44100);
-	utt_assert(v.getSize() == 6077595);
-	utt_assert(v.getWidth() == 532);
-	utt_assert(v.getHeight() == 300);
-	utt_assert(v.getDuration() > 84);
-	utt_assert(v.getDuration() < 85);
-	utt_assert(video::ffmpeg::thumbnail(v, 1));
-}
+	utt(test_video) {
+		String folder = "tests";
+		String filename = "Lion.ogv";
+		video::Video v(sys::path::join(folder, filename));
+		utt_assert(v.getFilename() == sys::path::join(folder, filename));
+		utt_assert(v.getAudioCodec() == "vorbis");
+		utt_assert(v.getVideoCodec() == "theora");
+		utt_assert(v.getFormat() == "Ogg");
+		utt_assert(v.getFrameRate() == 25);
+		utt_assert(v.getSampleRate() == 44100);
+		utt_assert(v.getSize() == 6077595);
+		utt_assert(v.getWidth() == 532);
+		utt_assert(v.getHeight() == 300);
+		utt_assert(v.getDuration() > 84);
+		utt_assert(v.getDuration() < 85);
+		utt_assert(video::ffmpeg::thumbnail(v, 1));
+	}
+
+	utt(test_video_folder) {
+		sys::Dir folder("res/video");
+		for (String entry: folder) {
+			if (entry.endsWith(".mp4")) {
+				String videoPath = sys::path::join("res/video", entry);
+				utt_assert(sys::path::isFile((const char*)videoPath));
+			}
+		}
+	}
 
 utt_end();
