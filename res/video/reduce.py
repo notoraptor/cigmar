764173a10e70a0c3512e52e32508042a4895ef89
notoraptor <stevenbocco@gmail.com>
2017-11-09 13:48:18 -0500

    Add videos for testing: https://videos.pexels.com/

---
+++ res/video/reduce.py
@@ -0,0 +1,19 @@
+""""
+Reduce size of videos which have extension ".mp4" in working directory.
+Use ffmpeg for resizing. Resized videos is written in sub-folder "reduced"
+(make sure this folder exists before running this script).
+Video ratio (expected 16/9) is kept, height is resized to 270 pixels.
+Make sure width can be divided by `n` when `height/n == 270`.
+
+To use the script:
+```
+python reduce.py > script.sh	# or scipr.bat on Windows
+./script.sh						# or `script.bat` on Windoed
+# On Unix, you may need to make script.sh runnable before using it (`chmod u+x script.sh`).
+```
+"""
+from __future__ import print_function
+import os
+paths = [name for name in os.listdir(".") if name.endswith(".mp4")]
+for name in paths:
+	print('ffmpeg -y -i %s -vf scale=-1:270 reduced/%s' % (name, name))
