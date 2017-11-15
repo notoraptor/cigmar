43e71d2dd115a4b633eb8fa86730c37d3e644692
notoraptor <stevenbocco@gmail.com>
2017-11-15 16:59:42 -0500

    [wip] integrate sqlite

---
+++ src/cigmar/math/Fraction.hpp
@@ -0,0 +1,65 @@
+//
+// Created by notoraptor on 15/11/2017.
+//
+
+#ifndef SRC_CIGMAR_FRACTION_HPP
+#define SRC_CIGMAR_FRACTION_HPP
+
+#include <cassert>
+#include <cstdint>
+#include <cigmar/interfaces/Streamable.hpp>
+
+namespace cigmar::math {
+	// Greatest common divisor ([fr] Plus Grand Commun Diviseur)
+	template<typename A, typename B>
+	A gcd(A a, B b) {
+		return a < b ? gcd(b, a) : (b == 0 ? a : gcd(b, a % b));
+	};
+
+	template<typename T>
+	class AbstractFraction: public Streamable {
+	private:
+		char s;
+		T num;
+		T den;
+		void update() {
+			T greatestCommonDivisor = gcd(num, den);
+			num /= greatestCommonDivisor;
+			den /= greatestCommonDivisor;
+		}
+	public:
+		AbstractFraction(char sign, T numerator, T denominator): s(sign), num(numerator), den(denominator) {
+			assert(s == 1 || s == -1);
+			assert(den != 0);
+			update();
+		};
+		AbstractFraction(int64_t numerator = 0, uint64_t denominator = 1) {
+			if (numerator < 0) {
+				s = -1;
+				num = -numerator;
+			} else {
+				s = 1;
+				num = numerator;
+			}
+			den = denominator;
+			assert(den != 0);
+			update();
+		};
+		explicit operator double() const {return s * (double)num/den;};
+		void toStream(std::ostream& o) const override {
+			if (num) {
+				if (s == -1) o << '-';
+				o << num;
+				if (den != 1) o << '/' << den;
+			} else {
+				// num == 0.
+				o << 0;
+			}
+		};
+	};
+
+	typedef AbstractFraction<uint64_t> Fraction;
+}
+
+
+#endif //SRC_CIGMAR_FRACTION_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <cigmar/print.hpp>
 #include <cigmar/file/writer/Binary.hpp>
 #include <video/database/Database.hpp>
+#include <cigmar/math/Fraction.hpp>
+#include <libraries/sqlite/sqlite3.h>
 
 using std::cout;
 using std::cerr;
@@ -24,7 +26,82 @@ const char* jstring = R"(
 
 */
 
+class SQLiteQuery {
+private:
+	sqlite3_stmt* stmt;
+	sqlite3* db;
+	bool done;
+	void next() {
+		int step = sqlite3_step(stmt);
+		if (step != SQLITE_ROW) {
+			if (step != SQLITE_DONE) {
+				String message = String::println("Error iterating SQL statement:", sqlite3_errmsg(db));
+				throw Exception(message);
+			}
+			done = true;
+		}
+	}
+	explicit SQLiteQuery(sqlite3* database, sqlite3_stmt* statement): db(database), stmt(statement), done(false) {
+		next();
+	};
+	friend class SQLiteDatabase;
+public:
+	~SQLiteQuery() {
+		if (stmt) sqlite3_finalize(stmt);
+	}
+	SQLiteQuery& operator++() {next();};
+	explicit operator bool() const {return !done;}
+	int getInt(int col);
+	int64_t getInt64(int col);
+	double getDouble(int col);
+	String getText(int col);
+	ArrayList<byte_t> getBlob(int col);
+};
+
+class SQLiteDatabase {
+private:
+	sqlite3* db;
+	void open(const char* dbname) {
+		if (db) throw Exception("This database object is already used.");
+		if (sqlite3_open(dbname, &db) != SQLITE_OK) {
+			String message = String::println("Cannot open database:", dbname, ":", sqlite3_errmsg(db));
+			sqlite3_close(db);
+			// TODO: Does sqlite3_close() really clear the db pointer ?
+			db = nullptr;
+			throw Exception(message);
+		}
+	};
+public:
+	SQLiteDatabase(): db(nullptr) {};
+	explicit SQLiteDatabase(const char* dbname): db(nullptr) {
+		open(dbname);
+	};
+	~SQLiteDatabase() {
+		if (db) sqlite3_close(db);
+	};
+	template<typename... Args>
+	SQLiteQuery run(const char* sql, Args&&... bindings) {
+
+	}
+};
+
 int main() {
+	sqlite3* db;
+	sqlite3_stmt* res;
+	int rc = sqlite3_open(":memory:", &db);
+	if (rc != SQLITE_OK) {
+		String s = String::print("Cannot open sqlite3:", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		throw Exception(s);
+	}
+	rc = sqlite3_prepare_v2(db, "SELECT SQLITE_VERSION()", -1, &res, 0);
+	if (rc != SQLITE_OK) {
+		String s = String::print("Cannot select sqlite version:", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		throw Exception(s);
+	}
+
+	/*
 	const char* foldername = "res/video";
 	const char* dbname = "mydb.db.tsv";
 	ArrayList<video::Video> videos = video::database::loadFromDirectory(foldername);
@@ -36,7 +113,11 @@ int main() {
 	for (const video::Video& video: videos) {
 		outfile << tsv.getRow(video) << ENDL;
 	}
+	math::Fraction f1, f2(0), f3(1), f4(-1), f5(32), f6(-64), f7(2, 3), f8(-15, 9), f9(-1, 12, 9), f10(1, 17, 69);
+	sys::err::println(f1, f2, f3, f4, f5, f6, f7, f8, f9, f10);
+	sys::err::println(math::Fraction(-777, 7*11*3));
 	tests::run();
+	*/
 	return 0;
 }
 
